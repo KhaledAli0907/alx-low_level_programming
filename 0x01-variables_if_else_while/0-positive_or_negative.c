84086ca16d8a44538dcaad9faa812192d3fcf7dc
KhaledAli0907 <khaledali0907@gmail.com>
2023-07-14 16:40:44 +0300

    0-positive_or_negative.c-fix#1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,10 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/* betty style doc for function main goes there 
+ * main: entery Point
+ * Return: (0) Success
+ * */
 int main(void)
 {
 	int n;
