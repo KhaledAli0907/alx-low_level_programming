18aed2c3d95a645e532a31b4ac0ad25b7fae80bd
KhaledAli0907 <khaledali0907@gmail.com>
2023-07-18 22:49:23 +0300

    9-times_table.c-fix#0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* main - Entry point
+* times_table - Entry point
 *
 * Return: Always 0 (Success)
 */
@@ -27,7 +27,7 @@ void times_table(void)
 					_putchar(' ');
 					_putchar(n + '0');
 				}
-				else if (col != 9)
+				if (col != 9)
 				{
 					_putchar(',');
 					_putchar(' ');
