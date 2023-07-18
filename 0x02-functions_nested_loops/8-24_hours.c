d7b7c730b6f84d5675bfc88fa621ab38cdc819e3
KhaledAli0907 <khaledali0907@gmail.com>
2023-07-18 21:58:41 +0300

    8-24_hours.c-fix#1

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,8 +9,8 @@
 void jack_bauer(void)
 {
 	int i, j;
-	
-	for (i = 0; i <= 24; i++)
+
+	for (i = 0; i < 24; i++)
 	{
 		for (j = 0; j < 60; j++)
 		{
@@ -22,5 +22,4 @@ void jack_bauer(void)
 			_putchar('\n');
 		}
 	}
-	_putchar('\n');
 }
