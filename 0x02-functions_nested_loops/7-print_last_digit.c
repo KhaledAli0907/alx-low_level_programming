0e57d3f814c0fc0a538a67f8e4b98f52b4e889ae
KhaledAli0907 <khaledali0907@gmail.com>
2023-07-18 20:49:05 +0300

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* print_last_digit - Entry point
+*
+* @n : Integer
+*
+* Return: Always 0 (Success)
+*/
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
---
+++ 0x02-functions_nested_loops/test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n = 495;
+	int i = n % 10;
+	printf("%i \n", i);
+	
+}
