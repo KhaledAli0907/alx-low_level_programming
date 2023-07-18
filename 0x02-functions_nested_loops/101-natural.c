ff87c4d4b85bf444b4a7d76b5e8ebe2ab707ae35
KhaledAli0907 <khaledali0907@gmail.com>
2023-07-19 00:37:29 +0300

    101-natural.c-fix#2

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,18 +2,18 @@
 #include <stdio.h>
 
 /**
-* natural - Entry point
+* main - Entry point
 *
 * Return: void
 */
 
-void natural(void)
+void main(void)
 {
 	int sum, i;
 
 	for (i = 0; i < 1024; i++)
-		if (i % 3 == 0 || i % 5 == 0)
+		if ((i % 3 == 0) || (i % 5 == 0))
 			sum += i;
 
-	printf("%i\n", sum);
+	printf("%d\n", sum);
 }
