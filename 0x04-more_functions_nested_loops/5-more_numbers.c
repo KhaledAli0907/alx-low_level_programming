f96f55e91280419bc1fcb64dedbe455ebe6a8943
KhaledAli0907 <khaledali0907@gmail.com>
2023-07-20 20:28:41 +0300

    task#5-fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,14 +8,13 @@ void more_numbers(void)
 {
 	int i, row;
 
-	for (row = 0; row <= 10; row++) /* print rows */
+	for (row = 0; row < 10; row++) /* print rows */
 	{
 		for (i = 0; i <= 14; i++) /* print numbers */
 		{
 			if (i > 9)
-			{
 				_putchar((i / 10) + '0');
-			}
+
 			_putchar((i % 10) + '0');
 		}
 		_putchar('\n');
