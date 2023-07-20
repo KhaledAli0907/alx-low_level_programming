ab276ba3147815aa15747724ebda5680857c2c85
KhaledAli0907 <khaledali0907@gmail.com>
2023-07-20 13:29:20 +0300

    task#6-fix#2

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -13,8 +13,12 @@ void print_line(int n)
 	if (n <= 0)
 		_putchar('\n');
 
-	for (i = 1; i <= n; i++)
-		_putchar('_');
-
-	_putchar('\n');
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
 }
