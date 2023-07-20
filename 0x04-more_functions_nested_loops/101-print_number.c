a016b86d7b7fc053daab8b2dd8d9376d11907a8f
KhaledAli0907 <khaledali0907@gmail.com>
2023-07-20 20:25:34 +0300

    101-print_number-fix

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,25 +1,26 @@
 #include "main.h"
 
 /**
-* main - Entry point
+* print_number - prints a number
 *
-* Return: Always 0 (Success)
+* @n : integer
 */
 
 void print_number(int n)
 {
 	unsigned int num = n;
+
 	if (n < 0)
 	{
 		/* change the number to positivie number and print the negative sign */
-		_putchar('-'); 
+		_putchar('-');
 		num = -num;
 	}
 
 	if ((num / 10) > 0)
 		/* print the first digits */
 		print_number(num / 10);
-	
+
 	/* print the last digit */
 	_putchar((num % 10) + '0');
 }
