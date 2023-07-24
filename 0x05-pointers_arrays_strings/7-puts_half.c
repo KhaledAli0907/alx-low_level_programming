10fe3732594aefd72e6f20aa03c754105620c29c
KhaledAli0907 <khaledali0907@gmail.com>
2023-07-25 01:21:46 +0300

    7-puts_half.c-fix

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -18,14 +18,14 @@ void puts_half(char *str)
 
 	if (len % 2 == 0)
 	{
-		for (n = len / 2; n <= len; n++)
+		for (n = len / 2; n < len; n++)
 		{
 			_putchar(str[n]);
 		}
 	}
 	else
 	{
-		for (n = (len - 1) / 2; n <= len; n++)
+		for (n = (len - 1) / 2; n < len; n++)
 		{
 			_putchar(str[n]);
 		}
