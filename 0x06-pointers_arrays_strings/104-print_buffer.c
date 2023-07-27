01ff7e2d547dcf838c773a7400d5a2e965354a75
KhaledAli0907 <khaledali0907@gmail.com>
2023-07-27 17:48:45 +0300

    task#12

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_buffer - prints a buffer
+*
+* @b: pointer to buffer
+* @size: size of the buffer
+*/
+
+void print_buffer(char *b, int size)
+{
+	int i = 0, j = 0;
+
+	while (i < size)
+	{
+		printf("%08x: ", i);
+		j = 0;
+		while (j < 10 && i + j < size)
+		{
+			printf("%02x ", b[i + j]);
+			j++;
+		}
+		while (j < 10)
+		{
+			printf("  ");
+			j++;
+		}
+		j = 0;
+		while (j < 10 && i + j < size)
+		{
+			if (b[i + j] >= ' ' && b[i + j] <= '~')
+				printf("%c", b[i + j]);
+			else
+				printf(".");
+			j++;
+		}
+		printf("\n");
+		i += 10;
+	}
+}
