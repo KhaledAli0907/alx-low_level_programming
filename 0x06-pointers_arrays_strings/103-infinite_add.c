c6ef2c5f2c1de7c2dabef3c91b017e2510f01998
KhaledAli0907 <khaledali0907@gmail.com>
2023-07-27 17:28:30 +0300

    task#11-fix#3

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+int _strlen(char *str);
+int compare(int x, int y);
 /**
 * infinite_add - Adds two numberes.
 *
@@ -13,25 +15,18 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int x = 0, y = 0, l1 = 0, l2 = 0, lar = 0, carry = 0, k = 0;
+	int x = 0, y = 0, l1 = 0, l2 = 0, lar = 0, carry = 0, k;
 
-	while (n1[l1] != '\0')
-		l1++;
-
-	while (n2[l2] != '\0')
-		l2++;
-	if (l1 > l2)
-		lar = l1;
-	else
-		lar = l2;
-	if (lar + 1 > size_r)
+	l1 = _strlen(n1);
+	l2 = _strlen(n2);
+	lar = compare(l1, l2);
+	if (lar + 1 >= size_r)
 		return (0);
 	r[lar] = '\0';
 	for (k = lar - 1; k >= 0; k--)
 	{
 		l1--;
 		l2--;
-
 		if (l1 >= 0)
 			x = n1[l1] - '0';
 		else
@@ -40,17 +35,52 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			y = n2[l2] - '0';
 		else
 			y = 0;
-		r[k] = ((x + y + carry) % 10) + '0';
+		r[k] = (x + y + carry) % 10 + '0';
 		carry = (x + y + carry) / 10;
 	}
 	if (carry == 1)
 	{
 		r[lar + 1] = '\0';
-		if (r[lar + 2] > size_r)
+		if (lar + 2 > size_r)
 			return (0);
 		while (lar-- >= 0)
 			r[lar + 1] = r[lar];
 		r[0] = carry + '0';
 	}
 	return (r);
+}
+
+
+/**
+ * _strlen - Return string's length
+ * @str: string.
+ * Return: string length
+ */
+
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+		i++;
+
+	return (i);
+}
+
+/**
+ * compare - compare two numbers
+ * @x: Integer.
+ * @y: Integer.
+ * Return: the largest number
+ */
+
+int compare(int x, int y)
+{
+	int largest;
+
+	if (x > y)
+		largest = x;
+	else
+		largest = y;
+	return (largest);
 }
