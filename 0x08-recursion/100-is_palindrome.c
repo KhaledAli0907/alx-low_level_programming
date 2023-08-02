5f04c3646ab130b8f026bf375d9351274826256e
KhaledAli0907 <khaledali0907@gmail.com>
2023-08-02 19:38:06 +0300

    added_recursion_instead_of_loops

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,23 +2,32 @@
 #include <string.h>
 
 /**
+ * check_palindrome - check if a string is polindrome or not using recursion
+ *
+ * @s: string
+ * @i: start
+ * @j: end
+ *
+ * Return 1 if string @s a palindrome, 0 otherwise.
+ */
+int check_palindrome(char *s, int i, int j)
+{
+	if (i >= j)
+		return (1);
+	if (s[i] != s[j])
+		return (0);
+	return (check_palindrome(s, i + 1, j - 1));
+}
+
+/**
  * is_palindrome - check if a string is polindrome or not
  *
  * @s: string
  *
  * Return: 1 if the string @s a polindrome, 0 otherwise.
  */
-
 int is_palindrome(char *s)
 {
-	int i = 0, j = strlen(s) - 1;
-
-	while (i < j)
-	{
-		if (s[i] != s[j])
-			return (0);
-		i++;
-		j--;
-	}
-	return (1);
+	
+	return (check_palindrome(s, 0, strlen(s) - 1));
 }
