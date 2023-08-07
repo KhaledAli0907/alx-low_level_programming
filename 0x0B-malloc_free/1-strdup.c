f691b2b470fe27e68aa4e9acfac4d6a6d62036c5
KhaledAli0907 <khaledali0907@gmail.com>
2023-08-07 09:05:25 +0300

    fixed_foldername

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - copy a string to newly alocated space in memory
+ *
+ * @str: string
+ *
+ * Return: a pointer to the newly allocated space
+ */
+
+char *_strdup(char *str)
+{
+	int i = 0, size = strlen(str);
+	char *strcp = NULL;
+
+	if (size == 0)
+		return (NULL);
+
+	strcp = malloc(sizeof(char) * size);
+
+	if (strcp == NULL)
+		return (NULL);
+
+	for (; i <= size; i++)
+		strcp[i] = str[i];
+
+	return (strcp);
+}
