5efd00a7dc422f9a7fde45e0dee05f78d9d264e9
KhaledAli0907 <khaledali0907@gmail.com>
2023-08-07 15:38:51 +0300

    task#100

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * argstostr - concatenates all argments
+ *
+ * @ac: number of args
+ * @ac: array of args
+ *
+ * Return: pointer to the concatenated args
+ */
+char *argstostr(int ac, int **av)
+{
+	int i = 0, j = 0, x = 0, len = 0;
+	char *arg = NULL;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	
+	/* get the length of the args */
+	for (i = 0; i < ac; i++)
+		for (j = 0; av[i][j]; j++)
+			len++;
+	len += ac;
+
+	arg = malloc(sizeof(char) * len + 1);
+	if (arg == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			/* else copy the char to the string */
+			arg[x] = av[i][j];
+			x++;
+		}
+		/* if we get to the null terminator swap with newline */
+		arg[x++] = '\n';
+	}
+	arg[x] = '\0';
+	return(arg);
+}
