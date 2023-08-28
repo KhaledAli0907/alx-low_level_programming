07afb375f1836cd21c8ce321ec732a4f27afc204
khaledali0907 <khaledali0907@gmail.com>
2023-08-28 21:33:43 +0300

    task#5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list
+ *
+ * @head: pointer to the first node
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp = NULL;
+
+	if (head == NULL || *head == NULL)
+		return;
+
+	while (*head != NULL)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+}
