9cbbc11ce9afb26d169f44b1c2ea6fbb33431c17
khaledali0907 <khaledali0907@gmail.com>
2023-08-31 21:13:42 +0300

    task#100

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * get_endianess - Checks the endianness
+ *
+ * Return - 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	int c = 1;
+
+	return (*(char *) &c);
+}
