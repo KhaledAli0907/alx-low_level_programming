1cf8a3e7915ec9d48538784dc0fe2c2f8b8b845d
khaledali0907 <khaledali0907@gmail.com>
2023-08-31 20:36:52 +0300

    task#4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * clear-bit - Sets a value of a bit to 0 at a givin index.
+ *
+ * @n: pointer to number
+ * @index: the givin index
+ * 
+ * Return: 1 if it worked, -1 otherwise.
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if ((sizeof(n) * 8) <= index)
+		return (-1);
+
+	*n = *n & ~(1l << index);
+	return (1);
+}
