573948163532095973ef5b3f502f23067eecae23
khaledali0907 <khaledali0907@gmail.com>
2023-09-04 16:52:48 +0300

    task#1-fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * create_file - Creates a file
@@ -10,11 +11,8 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	ssize_t len = 0, bytes = 0;
+	ssize_t len = strlen(text_content), bytes = 0;
 	int fp = 0;
-
-	while (*text_content++)
-		len++;
 
 	if (!filename)
 		return (-1);
