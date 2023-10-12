cd74eb454c9d0a5435f3e994a2ff7fa4dcbaf9fe
khaledali0907 <khaledali0907@gmail.com>
2023-10-12 14:20:17 +0300

    task#2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,15 +1,23 @@
-#include "list.h"
+#include "lists.h"
 
+/**
+ * add_dnodeint - inserts node add the beginning of a list
+ *
+ * @head: header
+ * @n: the element to be added
+ *
+ * Return: pointer to the newly created node
+ */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *node = NULL;
-	
+
 	node = malloc(sizeof(dlistint_t));
 	if (node == NULL)
 		return (NULL);
 
 	node->n = n;
-	node->next = h->prev;
+	node->next = *head;
 	node->prev = NULL;
 	*head = node;
 
