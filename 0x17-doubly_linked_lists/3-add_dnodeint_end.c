83fc8fde3091fc8122a26e7cec43becb6dee1ba9
khaledali0907 <khaledali0907@gmail.com>
2023-10-12 15:23:16 +0300

    task#3

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,5 +1,13 @@
 #include "lists.h"
 
+/**
+ * add_nodeint_end - add a node in the end of a list
+ *
+ * @head: pointer to the head of list
+ * @n: elemnt to be added to the newly created node
+ *
+ * Return: pointer to the created node
+ */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *node = NULL, *temp = *head;
