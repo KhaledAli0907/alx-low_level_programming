b08f354c8514ca60be3b44e8736b6db770d48083
khaledali0907 <khaledali0907@gmail.com>
2023-11-03 18:11:56 +0200

    task#0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hash_tables"
+
+/**
+ * hash_table_create - Creates a hash table
+ *
+ * @size: size of the table
+ * Return: pointer to the newly created table
+ * Null otherwise
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i = 0;
+	hash_table_t *ht = NULL;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_table_t) * size);
+	if (ht->array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
+
+	return (ht);
+}
